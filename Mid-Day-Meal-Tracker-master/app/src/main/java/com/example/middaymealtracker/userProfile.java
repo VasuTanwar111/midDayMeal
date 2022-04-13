d53e4b736627b80c057a40c8600c7c15e0820c02
Vasu <vasutanwar11@gmail.com>
2022-04-13 11:34:13 +0530

    First commit

---
+++ Mid-Day-Meal-Tracker-master/app/src/main/java/com/example/middaymealtracker/userProfile.java
@@ -0,0 +1,14 @@
+package com.example.middaymealtracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class userProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
